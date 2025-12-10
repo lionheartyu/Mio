15d31dc606412cf726fd5856a0a581117ae69b2e
dongshengyuan <dongshengyuan@uniontech.com>
2025-12-10 17:09:47 +0800

    add-annotation-1

--- KmuDuo/noncopyable.h
+++ KmuDuo/noncopyable.h
@@ -8,10 +8,10 @@ noncopyable被继承以后，派生类对象可以正常进行构造和析构，
 class noncopyable
 {
 public:
-    noncopyable(const noncopyable &) = delete;
-    noncopyable &operator=(const noncopyable) = delete;
+    noncopyable(const noncopyable &) = delete; // 禁用拷贝构造函数，防止对象被拷贝
+    noncopyable &operator=(const noncopyable) = delete; // 禁用赋值操作符，防止对象被赋值
 
 protected:
-    noncopyable() = default;
-    ~noncopyable() = default;
+    noncopyable() = default;  // 允许默认构造
+    ~noncopyable() = default; // 允许默认析构
 };
